bfd8a02b827f4a7d3d7c38b69d0930a2a3e931ec
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-02-26 22:52:22 +0500

    Вынес компонент PopupWithForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Header from '../components/Header.js';
 import Main from '../components/Main.js';
 import Footer from '../components/Footer.js';
+import PopupWithForm from '../components/PopupWithForm.js';
 
 function App() {
   return (
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,16 @@
+function PopupWithForm({title, name, children}) {
+    return (
+        <section className={`popup popup_type_${name}`}>
+        <div className="popup__container">
+          <button className="popup__close" type="button"></button>
+          <h2 className="popup__title">{title}</h2>
+          <form className="popup__form popup__form_type_profile" name={`${name}`} novalidate>
+            {children}
+            <button className="popup__save" type="submit">Сохранить</button>
+          </form>
+        </div>
+      </section>
+    );
+}
+
+export default PopupWithForm;
