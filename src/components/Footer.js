3dbdfab8e1277ce1ad561d3d0de192f52b557de2
Yakov Zilberman <113699485+Argayash1@users.noreply.github.com>
2023-03-02 10:53:04 +0500

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer__copyright">&copy; 2023 Mesto Russia</p>
-        </footer>
-    );
-}
-
-export default Footer;
