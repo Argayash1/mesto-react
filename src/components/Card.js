ccb716e8e39851813cd045b2d69f30d550883f16
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-03-10 02:23:35 +0500

    Подписал на контекст компонент Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,29 @@
-function Card({card, onCardClick, onDeleteCardClick}) {
+import { useContext } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
+
+function Card({ card, onCardClick, onDeleteCardClick }) {
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = (
+    `card__like-button ${isLiked && 'card__like-button_active'}`
+  );;
+
   function handleClick() {
     onCardClick(card);
   }
 
-    return (
-        <li className="element">
-        <button className="element__delete-button" type="button" onClick={onDeleteCardClick}></button>
-        <img className="element__image" src={card.link} alt={card.name} onClick={handleClick}/>
-        <div className="element__info">
-          <h3 className="element__title">{card.name}</h3>
-          <button className="element__like-button" type="button"></button>
-          <span className="element__count-likes">{card.likes.length}</span>
-        </div>
-      </li>
-    )
+  return (
+    <li className="element">
+      {isOwn && <button className="element__delete-button" type="button" onClick={onDeleteCardClick}></button>}
+      <img className="element__image" src={card.link} alt={card.name} onClick={handleClick} />
+      <div className="element__info">
+        <h3 className="element__title">{card.name}</h3>
+        <button className="element__like-button" type="button"></button>
+        <span className="element__count-likes">{card.likes.length}</span>
+      </div>
+    </li>
+  )
 }
 
 export default Card;
