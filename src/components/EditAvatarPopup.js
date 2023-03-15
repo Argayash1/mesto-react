5ae8f1013daaae1a1991ac7bcc0e61b870398f2d
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-03-15 20:24:42 +0500

    Сделал валидацию для всех попапов

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,52 +1,53 @@
-import { useRef, useState } from 'react'
+import { useRef, useEffect } from 'react'
+import useValidation from '../components/useValidation.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 
-function EditAvatarPopup({isOpen, onClose, onCloseByClickOnOverlay, onUpdateAvatar, isLoading, loadingText}) {
-    const ref = useRef(); // записываем объект, возвращаемый хуком, в переменную
-    const [isValid, setIsValid] = useState(false);
+function EditAvatarPopup({ isOpen, onClose, onCloseByClickOnOverlay, onUpdateAvatar, isLoading, loadingText }) {
+  const ref = useRef(); // записываем объект, возвращаемый хуком, в переменную
+  const { values, errors, formValid, onChange, resetValidation } = useValidation();
 
-    function inputValidation() {
-      if (!ref.validity.valid) {
-        setIsValid(true)
-      } else {
-        setIsValid(false)
-      }
-    };
-    
-    function handleSubmit(e) {
-        e.preventDefault();
-      
-        onUpdateAvatar({
-          avatar: ref.current.value
-        });
+  useEffect(() => {
+    resetValidation();
+  }, [isOpen, resetValidation]);
 
-        setTimeout(() => {
-          ref.current.value = '';
-      }, 1500)
-      }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    return (
-        <PopupWithForm
-        title="Обновить аватар"
-        name="new-avatar"
-        isOpen={isOpen}
-        onClose={onClose}
-        onCloseByClickOnOverlay={onCloseByClickOnOverlay}
-        onSubmit={handleSubmit}
-        submitButtonText={isLoading ? loadingText : "Сохранить"}
-      >
-        <input
-          type="url"
-          ref={ref}
-          id="image-url"
-          className="popup__input"
-          name="url"
-          placeholder="Ссылка на картинку"
-          autoComplete="off"
-          required />
-        <span className="popup__error" id="image-url-error"></span>
-      </PopupWithForm>
-    )
+    onUpdateAvatar({
+      avatar: ref.current.value
+    });
+
+    setTimeout(() => {
+      ref.current.value = '';
+    }, 1500)
+  }
+
+  return (
+    <PopupWithForm
+      title="Обновить аватар"
+      name="new-avatar"
+      isOpen={isOpen}
+      onClose={onClose}
+      onCloseByClickOnOverlay={onCloseByClickOnOverlay}
+      onSubmit={handleSubmit}
+      submitButtonText={isLoading ? loadingText : "Сохранить"}
+      isLoading={isLoading}
+      isValid={!formValid}
+    >
+      <input
+        type="url"
+        ref={ref}
+        value={values.url || ''}
+        onChange={onChange}
+        id="image-url"
+        className="popup__input"
+        name="url"
+        placeholder="Ссылка на картинку"
+        autoComplete="off"
+        required />
+      <span className={`popup__error ${errors.url !== "" && "popup__error_visible"}`} id="image-url-error">{errors.url}</span>
+    </PopupWithForm>
+  )
 }
 
 export default EditAvatarPopup;
